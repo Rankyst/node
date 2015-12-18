5aabfcb6ed4204d34e3035aa08e1f999565e9cb7
Jackson Tian <shyvo1987@gmail.com>
2015-12-18 10:57:21 +0800

    buffer: faster case for create buffer from empty string
    When create Buffer from empty string will touch
    C++ binding also.
    
    This patch can improve edge case 2x faster.
    following is benchmark results.
    
    before:
    buffers/buffer_zero.js n=1024 type=buffer: 2332.47578
    buffers/buffer_zero.js n=1024 type=string: 1213.07292
    
    after:
    buffers/buffer_zero.js n=1024 type=buffer: 2341.33736
    buffers/buffer_zero.js n=1024 type=string: 2219.30448

--- benchmark/buffers/buffer_zero.js
+++ benchmark/buffers/buffer_zero.js
@@ -3,16 +3,18 @@
 const common = require('../common.js');
 
 const bench = common.createBenchmark(main, {
-  n: [1024]
+  n: [1024],
+  type: ['buffer', 'string']
 });
 
-const zero = new Buffer(0);
+const zeroBuffer = new Buffer(0);
+const zeroString = '';
 
 function main(conf) {
   var n = +conf.n;
   bench.start();
   for (let i = 0; i < n * 1024; i++) {
-    new Buffer(zero);
+    conf.type === 'buffer' ? new Buffer(zeroBuffer) : new Buffer(zeroString);
   }
   bench.end(n);
 }
