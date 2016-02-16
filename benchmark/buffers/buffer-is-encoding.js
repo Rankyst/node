e12a2aec4a0b4a33db131769cd7239ba7272fe73
Jackson Tian <shyvo1987@gmail.com>
2016-02-16 16:01:59 +0800

    buffer: speed up Buffer.isEncoding() method
    Use automata to avoid toLowerCase(), faster, but more dirty.

---
+++ benchmark/buffers/buffer-is-encoding.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const common = require('../common.js');
+const v8 = require('v8');
+
+const bench = common.createBenchmark(main, {
+  encoding: [
+    'hex',
+    'utf8',
+    'utf-8',
+    'ascii',
+    'binary',
+    'base64',
+    'ucs2',
+    'ucs-2',
+    'utf16le',
+    'utf-16le',
+    'HEX',
+    'UTF8',
+    'UTF-8',
+    'ASCII',
+    'BINARY',
+    'BASE64',
+    'UCS2',
+    'UCS-2',
+    'UTF16LE',
+    'UTF-16LE',
+    'utf9',
+    'utf-7',
+    'utf17le',
+    'utf-17le',
+    'Unicode-FTW',
+    'new gnu gun'
+  ]
+});
+
+function main(conf) {
+  var encoding = conf.encoding;
+  // Force optimization before starting the benchmark
+  Buffer.isEncoding(encoding);
+  v8.setFlagsFromString('--allow_natives_syntax');
+  eval('%OptimizeFunctionOnNextCall(Buffer.isEncoding)');
+  Buffer.isEncoding(encoding);
+
+  bench.start();
+  var n = 1024 * 1024;
+  for (let i = 0; i < n; i++) {
+    Buffer.isEncoding(encoding);
+  }
+  bench.end(n);
+}
